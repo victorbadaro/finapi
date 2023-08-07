72454f77a204b7ae1709ec6b4c4d8dd68ad5ebf7
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-08-07 12:03:57 -0300

    feat: add functions to add a new account and to return the account statement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+app.use(express.json());
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      error: 'Customer already exists!'
+    });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send();
+});
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+});
 
 app.listen(3333);
